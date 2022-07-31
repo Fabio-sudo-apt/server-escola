d0e1c06d7ca22753720fcd0954ce85271992696d
fabio-sudo-apt <fsfreita1190@gmail.com>
2022-07-31 13:57:07 -0300

    config port

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import ip from 'ip';
 
 require('dotenv').config()
 
-const port = process.env.PORT
-const http = process.env.HTTP
+const port = process.env.PORT || 3030
+const http = process.env.HTTP || 'http'
 const address = ip.address()
 
 app.listen(port, () => {
