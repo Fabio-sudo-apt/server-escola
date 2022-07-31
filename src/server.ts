4b71ee066020fe7ed9a5c48d82243c8ec254d59b
fabio-sudo-apt <fsfreita1190@gmail.com>
2022-07-31 15:21:03 -0300

    add port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,7 @@
 import app from "./App";
-import ip from 'ip';
 
 require('dotenv').config()
 
 const port = process.env.PORT || 3030
-const http = process.env.HTTP || 'http'
-const address = ip.address()
 
-app.listen(port, () => {
-    console.log(`Server on: ${http}://${address}:${port}`)
-})
+app.listen(port)
